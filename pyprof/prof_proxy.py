330b50a62a9fa801e96de814fe75856d8848244d
lizeyan <li_zeyan@icloud.com>
2021-05-17 17:34:10 +0800

    update profile proxy

--- pyprof/prof_proxy.py
+++ pyprof/prof_proxy.py
@@ -1,7 +1,8 @@
+import types
 from collections import defaultdict
 from functools import wraps
 from threading import Thread, current_thread
-from typing import overload, Callable
+from typing import overload, Callable, Any
 
 from .pyprof import Profiler
 
@@ -9,8 +10,9 @@ from .pyprof import Profiler
 class ProfilerProxy:
     active_proxy: dict[Thread, list[tuple["ProfilerProxy", "Profiler"]]] = defaultdict(list)
 
-    def __init__(self, name: str):
+    def __init__(self, name: str, report_printer: Callable[[str], Any] = None):
         self.name = name
+        self.report_printer = report_printer
 
     def __enter__(self):
         current_stack = self.active_proxy[current_thread()]
@@ -25,6 +27,8 @@ class ProfilerProxy:
     def __exit__(self, exc_type, exc_val, exc_tb):
         _, profiler = self.active_proxy[current_thread()].pop()
         profiler.__exit__(exc_type, exc_val, exc_tb)
+        if self.report_printer is not None:
+            self.report_printer(profiler.report())
 
     def __call__(self, func: Callable):
         @wraps(func)
@@ -36,7 +40,7 @@ class ProfilerProxy:
 
 
 @overload
-def profile(name: str) -> Callable:
+def profile(name: str, report_printer=None) -> Callable:
     ...
 
 
@@ -45,15 +49,17 @@ def profile(func: Callable) -> Callable:
     ...
 
 
-def profile(arg: str or Callable) -> Callable:
+def profile(arg: str or Callable, report_printer=None) -> Callable:
     if isinstance(arg, str):
-        return ProfilerProxy(arg)
+        return ProfilerProxy(arg, report_printer=report_printer)
 
     func = arg
 
     @wraps(func)
     def wrapper(*args, **kwargs):
-        with ProfilerProxy(func.__name__):
+        with ProfilerProxy(
+                func.__qualname__
+        ):
             return func(*args, **kwargs)
 
     return wrapper
